f2a3b936556795d87caf097c6961446d00a0cc77
Toby HUANG <tecty@localhost.localdomain>
2018-08-20 14:34:25 +1000

    modify the serilizer structure. Generation of priority list

--- whip/serializers.py
+++ whip/serializers.py
@@ -2,8 +2,36 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 
 
-# serializer of user model 
+# serializer of user models
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ('url', 'username', 'email', 'is_staff')
+
+class PostSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Post
+        fields = (
+            'title',
+            "renovaters" ,
+            "date" ,
+            "eventType" ,
+            "location" ,
+            "peopleCount" ,
+            "ageProfolio" ,
+            "kitchenType" ,
+            "provideCookery" ,
+            "budget" ,
+            "extraCriteria",
+            "state"
+        )
+
+class CriteriaSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Criteria
+        fields = ('key','value')
+
+class TakerSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Taker 
+        fields = ('post','bidder','offer')

78d8b1b8743a78ef879530b8ce7c24d9a62370f3
zhilu1 <z5096599@student.unsw.edu.au>
2018-08-26 16:58:37 +1000

    base front-end using vuetify

---
+++ partywhip-frontend/src/components/AppHome.vue
@@ -0,0 +1,38 @@
+<template>
+   <v-content>
+  <v-container fluid>
+    <v-slide-y-transition mode="out-in">
+      <v-layout column align-center fill-height>
+        <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
+        <blockquote>
+          &#8220;This is Home page.&#8221;
+          <footer>
+            <small>
+              <em>&mdash;John Johnson</em>
+            </small>
+          </footer>
+        </blockquote>
+      </v-layout>
+    </v-slide-y-transition>
+  </v-container>
+   </v-content>
+
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ partywhip-frontend/src/components/AppPost.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-content>
+  <v-container fluid>
+    <v-slide-y-transition mode="out-in">
+      <v-layout column align-center fill-height>
+        <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
+        <blockquote>
+          &#8220;This is Post page.&#8221;
+        </blockquote>
+      </v-layout>
+    </v-slide-y-transition>
+  </v-container>
+    </v-content>
+</template>

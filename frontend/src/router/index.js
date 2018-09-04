49e57705419640300c88d1149fddaadec0eced79
Toby HUANG <619058900@qq.com>
2018-09-05 01:37:22 +1000

    success acquire sperate file of route.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -7,52 +7,56 @@ import bid from "./bid";
 
 Vue.use(Router);
 
+// main route list
+var routeLists = [
+  {
+    path: "/",
+    name: "home",
+    component: AppHome,
+    meta: {
+      // key to let the view can be view from guest
+      guest: true
+    }
+  },
+  {
+    path: "/about",
+    name: "about",
+    component: () => import("@/views/About.vue"),
+    meta: {
+      // key to let the view can be view from guest
+      guest: true
+    }
+  },
+  {
+    path: "/login",
+    name: "login",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */
+      "@/views/Login.vue"),
+    meta: {
+      // key to let the view can be view from guest
+      guest: true
+    },
+    beforeEnter: (to, from, next) => {
+      // if user is currently logged in, prevent him from hitting this page.
+      if (isLogin()) {
+        next("/");
+      } else {
+        // go to next
+        next();
+      }
+    }
+  }
+];
+
+// acquire the routes sotre in seperate files
+routeLists.concat(post, bid);
+
 export default new Router({
   mode: "history",
   base: process.env.BASE_URL,
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: AppHome,
-      meta: {
-        // key to let the view can be view from guest
-        guest: true
-      }
-    },
-    {
-      path: "/about",
-      name: "about",
-      component: () => import("@/views/About.vue"),
-      meta: {
-        // key to let the view can be view from guest
-        guest: true
-      }
-    },
-    {
-      path: "/login",
-      name: "login",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */
-        "@/views/Login.vue"),
-      meta: {
-        // key to let the view can be view from guest
-        guest: true
-      },
-      beforeEnter: (to, from, next) => {
-        // if user is currently logged in, prevent him from hitting this page.
-        if (isLogin()) {
-          next("/");
-        } else {
-          // go to next
-          next();
-        }
-      }
-    },
-    post,
-    bid
-  ]
+  routes: routeLists
 });

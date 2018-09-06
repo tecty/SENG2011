bec6b1cfe33726ffe11fca706b50b5a6f1814e9f
Toby HUANG <619058900@qq.com>
2018-09-06 17:13:15 +1000

    aa

--- frontend/src/views/post/Show.vue
+++ frontend/src/views/post/Show.vue
@@ -1,60 +1,18 @@
 <template>
-  <v-layout mt-3>
-    <v-slide-y-transition mode="out-in">
-      <v-layout row>
-        <v-flex xs12 sm8 offset-sm2>
-          <v-card>
-            <v-toolbar color="light-blue" dark>
-              <v-toolbar-side-icon></v-toolbar-side-icon>
-
-              <v-toolbar-title>Tasks</v-toolbar-title>
-
-              <v-spacer></v-spacer>
-
-              <v-btn icon>
-                <v-icon>search</v-icon>
-              </v-btn>
-
-              <v-btn icon>
-                <v-icon>view_module</v-icon>
-              </v-btn>
-            </v-toolbar>
-            <v-expansion-panel two-line focusable>
-              <v-expansion-panel-content v-for="post in posts" :key="post.id" avatar>
-                <div slot="header">{{ post.id }}{{ post.msg }}</div>
-                <v-card>
-                  <v-card-title>
-                    <div>
-                      <span class="grey--text">{{ post.title }}</span><br>
-                      <span>{{ post.location }}</span><br>
-                      <span>{{ post.poster.username }}</span><br>
-                      <span>{{ post.peopleCount }}</span><br> ...
-                    </div>
-                  </v-card-title>
-                  <v-card-actions>
-                    <v-btn flat color="orange" 
-                    :to="{ name:'BidCreate', params:{postId: post.id} }" >Bid for it</v-btn>
-                  </v-card-actions>
-                </v-card>
-              </v-expansion-panel-content>
-            </v-expansion-panel>
-          </v-card>
-        </v-flex>
-      </v-layout>
-    </v-slide-y-transition>
-    <v-btn fixed bottom right fab dark color="red" to="/post/create">
-      <v-icon dark>add</v-icon>
-    </v-btn>
-  </v-layout>
-
+  <v-container fluid grid-list-md>
+    <v-data-iterator :items="posts" content-tag="v-layout" row wrap>
+      <v-flex slot="item" slot-scope="props" sm12 sm6 md4 lg3 >
+        <!-- actual data is iterating at this v-flex layer -->
+        <postCard :post="props.item"/>
+      </v-flex>
+    </v-data-iterator>
+  </v-container>
 </template>
 
 
 <script>
 import axios from "axios";
-var sessionUrl = "http://127.0.0.1:8000/api-v0/posts/";
-var uname = "zhilu";
-var pass = "123456";
+import postCard from "@/components/post/Card";
 export default {
   data() {
     return {
@@ -63,16 +21,7 @@ export default {
   },
   mounted() {
     axios
-      .get(
-        sessionUrl,
-        {},
-        {
-          auth: {
-            username: uname,
-            password: pass
-          }
-        }
-      )
+      .get("posts/")
       .then(response => {
         // JSON responses are automatically parsed.
         console.log(response);
@@ -82,6 +31,9 @@ export default {
         console.log(error);
         console.log(error.response);
       });
+  },
+  components: {
+    postCard
   }
 };
 </script>
@@ -90,22 +42,4 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1,
-h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>

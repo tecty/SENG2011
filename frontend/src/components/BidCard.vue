e240d349fe59b689c482a745f168400ae44a2911
zhilu1 <z5096599@student.unsw.edu.au>
2018-10-06 20:12:23 +1000

    finish the functionality

--- frontend/src/components/BidCard.vue
+++ frontend/src/components/BidCard.vue
@@ -10,13 +10,12 @@
         </v-card-title>
         <v-card-actions>
           <div v-if="post.event.owner.username == currUser">
-            <v-btn flat dark @click="updatePostStatus()">Choose</v-btn>
+            <v-btn flat dark @click="chooseBid()">Choose</v-btn>
           </div>
           <v-spacer></v-spacer>
           <v-btn flat color="orange">Comment</v-btn>
         </v-card-actions>
       </v-card>
-          {{this.post}}
     </v-flex>
   </v-layout>
 
@@ -33,19 +32,9 @@ import { mapActions, mapState } from "vuex";
       };
     },
     methods: {
-      ...mapActions(["updatePost"]),
-      updatePostStatus() {
-        this.post.state = "SD"; // bidding => selected
-        // var data = {
-        //   id: this.post.id,
-        //   title: this.post.title,
-        //   message: this.post.message,
-        //   budget: this.post.budget,
-        //   peopleCount: this.post.peopleCount,
-        //   extraParameter: this.post.extraParameter // TODO extraparameter
-        // }
+      chooseBid() {
         axios
-        .patch("posts/" + this.post.id + "/", {state: "SD"})
+        .post("posts/" + this.post.id + "/choose/", {id: this.bid.id})
           .then(response => {
             // JSON responses are automatically parsed.
             console.log(response);
@@ -55,15 +44,6 @@ import { mapActions, mapState } from "vuex";
             console.log(error);
             console.log(error.response);
           });
-      //   this.updatePost(this.post.id,this.post)
-      //   .then((response) => {
-      //           console.log(response);
-      //         })
-      //         .catch((err) => {
-      //           this.error = "import is not correct";
-      //           console.log(err);
-      //         });
-      // }
       }
     },
   };
